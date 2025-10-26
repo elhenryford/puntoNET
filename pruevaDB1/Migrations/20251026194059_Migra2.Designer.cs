69fe9b6894c7d92a06aab5685fb71d98ec745ecc
Frankb-eng <franciscoblazevic@hotmail.com>
2025-10-26 16:46:48 -0300

    Crea en la db

--- pruevaDB1/Migrations/20251026194059_Migra2.Designer.cs
+++ pruevaDB1/Migrations/20251026194059_Migra2.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using pruevaDB1.Data;
 
@@ -11,9 +12,11 @@ using pruevaDB1.Data;
 namespace pruevaDB1.Migrations
 {
     [DbContext(typeof(pruevaDB1Context))]
-    partial class pruevaDB1ContextModelSnapshot : ModelSnapshot
+    [Migration("20251026194059_Migra2")]
+    partial class Migra2
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
