d3a254ae057b7f0df7abe2a042f6a920655f5a5f
Henry Ford <henry.ford@estudiantes.utec.edu.uy>
2025-10-27 18:58:15 -0300

    Andando con BD configurada

---
+++ ProyectoPuntoNET/Migrations/ProyectoPuntoNET/20251027210254_InitialCreate.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProyectoPuntoNET.Migrations.ProyectoPuntoNET
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Evento",
+                columns: table => new
+                {
+                    EventoId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombre = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Evento", x => x.EventoId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Atleta",
+                columns: table => new
+                {
+                    Numero = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Nombre = table.Column<string>(type: "TEXT", nullable: false),
+                    Edad = table.Column<int>(type: "INTEGER", nullable: false),
+                    ChipId = table.Column<string>(type: "TEXT", nullable: true),
+                    Categoria = table.Column<string>(type: "TEXT", nullable: true),
+                    EventoId = table.Column<int>(type: "INTEGER", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Atleta", x => x.Numero);
+                    table.ForeignKey(
+                        name: "FK_Atleta_Evento_EventoId",
+                        column: x => x.EventoId,
+                        principalTable: "Evento",
+                        principalColumn: "EventoId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Atleta_EventoId",
+                table: "Atleta",
+                column: "EventoId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Atleta");
+
+            migrationBuilder.DropTable(
+                name: "Evento");
+        }
+    }
+}
