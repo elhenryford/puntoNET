48f5a8a53fa6326687ddaeeb80d44b701c5fe8a9
Frankb-eng <franciscoblazevic@hotmail.com>
2025-11-04 17:53:14 -0300

    Ahi si

--- pruevaDB1/obj/Debug/net8.0/pruevaDB1.AssemblyInfo.cs
+++ pruevaDB1/obj/Debug/net8.0/pruevaDB1.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("pruevaDB1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6098991a2eea6a6795fbfed8fbfe04d86fe312a9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+87182499fa23574ebf1b23aa5e032ba344c6c138")]
 [assembly: System.Reflection.AssemblyProductAttribute("pruevaDB1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("pruevaDB1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
