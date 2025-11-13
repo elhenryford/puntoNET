2be76a0e26de7ad8fe95eb65d1f5cbf049df93bc
Frankb-eng <franciscoblazevic@hotmail.com>
2025-11-13 19:23:00 -0300

    asd

---
+++ pruevaDB1/Components/Model/Carrera.cs
@@ -0,0 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace pruevaDB1.Components.Model
+{
+    public class Carrera
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int IdCarrera { get; set; }
+        [Required]
+        public string Nombre { get; set; } = string.Empty;
+        public DateTime Fecha { get; set; }
+        public DateTime HoraInicio { get; set; }
+        public string Ubicacion { get; set; } = string.Empty;
+        public int cantSensores { get; set; }
+        public int inscGanador { get; set; } = 0;
+        public ICollection<Inscripcion> Inscripciones { get; set; } = new List<Inscripcion>();
+    }
+}
---
+++ pruevaDB1/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using pruevaDB1.Components.Model;
+using pruevaDB1.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Base de datos
+builder.Services.AddDbContextFactory<pruevaDB1Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+// Cola registrada correctamente
+builder.Services.AddSingleton<QueueService>();
+builder.Services.AddHostedService(provider => provider.GetRequiredService<QueueService>());
+
+builder.Services.AddControllers();
+
+// Blazor .NET 8
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+builder.Services.AddSignalR();
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddAntiforgery();
+
+builder.Services.AddHttpClient("API", client =>
+{
+    client.BaseAddress = new Uri("https://localhost:7111/");
+});
+
+builder.Services.AddScoped(sp =>
+    sp.GetRequiredService<IHttpClientFactory>().CreateClient("API"));
+
+
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAntiforgery();
+
+// ✅ ÚNICO estilo Blazor en .NET 8
+app.MapRazorComponents<pruevaDB1.Components.App>()
+    .AddInteractiveServerRenderMode();
+app.MapControllers();
+
+app.Run();
